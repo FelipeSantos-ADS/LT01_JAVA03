d160f9dee04caf7b57957a8dba81dae63ea4538a
FelipeSantos-ADS <felipesilveira302007@gmail.com>
2026-09-23 21:42:09 -0300

    Display series result incrementally in main method
    Moved the print statement inside the loop to display the series result incrementally.

--- LT01_ESTREP33.java
+++ LT01_ESTREP33.java
@@ -6,8 +6,8 @@ public class LT01_ESTREP33 {
         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
         for(int i = 1; i < (numero + 1); i++){
             serie = serie + (1.0/i);
-        }
-        System.out.println("O resultado da série é: " + serie);
+            System.out.println("O resultado da série é: " + serie);
+        }   
     }
     
 }
